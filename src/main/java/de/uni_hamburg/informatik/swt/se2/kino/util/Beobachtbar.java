58e8ad6f88e86c25e5c523a64efc7b68a62610d6
Benokit4 <ben.denikus@studium.uni-hamburg.de>
2024-06-03 16:17:28 +0200

    Implementiere Beobachtbar

---
+++ src/main/java/de/uni_hamburg/informatik/swt/se2/kino/util/Beobachtbar.java
@@ -0,0 +1,38 @@
+package de.uni_hamburg.informatik.swt.se2.kino.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Abstrakte Beobachtbar-Klasse für das Observer-Pattern
+ * 
+ * @author Ben Denikus
+ * @version 03.06.2024
+ */
+public abstract class Beobachtbar 
+{
+	private final List<Beobachter> _beobachter;
+	
+	protected Beobachtbar()
+	{
+		_beobachter = new ArrayList<>();
+	}
+	
+	/**
+	 * Registriert den Beobachter und fügt diesen zur Liste hinzu.
+	 * 
+	 * @param beobachter Der Beobachter
+	 */
+	public void registriereBeobachter(Beobachter beobachter)
+	{
+		_beobachter.add(beobachter);
+	}
+	
+	/**
+	 * Es werden alle Beobachter ueber alle Aenderungen informiert.
+	 */
+	protected void informiereUeberAenderung()
+	{
+		_beobachter.forEach(Beobachter::reagiereAufAenderung);
+	}
+}
