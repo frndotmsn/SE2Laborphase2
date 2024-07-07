1bb6eb45a61ced126d6204145f77b761eac63344
YonasUHH <jonas.schwanzer@studium.uni-hamburg.de>
2024-07-07 22:51:15 +0200

    Implementation fuer BarzahlungView

--- src/main/java/de/uni_hamburg/informatik/swt/se2/kino/ui/barzahlung/BarzahlungView.java
+++ src/main/java/de/uni_hamburg/informatik/swt/se2/kino/ui/barzahlung/BarzahlungView.java
@@ -18,7 +18,7 @@ import javax.swing.SwingConstants;
 import de.uni_hamburg.informatik.swt.se2.kino.wertobjekte.Geldbetrag;
 
 /**
- * Das UI des {@link BarzahlungController}.
+ * Das UI des {@link BarzahlungView}.
  * 
  * @author Jonas Schwanzer
  * @version SoSe 2024
@@ -27,17 +27,27 @@ public class BarzahlungView
 {
     // Die Widgets, aus denen das UI sich zusammensetzt
     private JDialog _dialog;
+    private JTextField _einzahlungTextField;
+    private JLabel _restbetragLabel;
+    private JLabel _fehlermeldungLabel;
+    private JLabel _preisWertLabel;
+    private JLabel _textRestbetragLabel;
+    private JLabel _textEinzahlungLabel;
+    private JLabel _textPreisLabel;
+    private JButton _bezahlenButton;
+    private JButton _abbrechenButton;
+    private JPanel _barzahlungPanel;
 
     /**
-     * Initialisert die Oberfläche. Die Parameter sind die Views der Submodule,
-     * die eingebettet werden.
+     * Initialisert die Bezahlungs-Oberflaeche und nimmt einen Geldbetrag
+     * entgegen, welcher gedeckt werden muss.
      */
     public BarzahlungView(Geldbetrag geldbetrag)
     {
         _dialog = new JDialog();
         _dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         _dialog.setResizable(false);
-        _dialog.setModal(false);
+        _dialog.setModal(true);
         _dialog.getContentPane()
             .setLayout(new BorderLayout());
 
@@ -58,7 +68,7 @@ public class BarzahlungView
      */
     public void zeigeFenster()
     {
-        _dialog.setSize(400, 300);
+        _dialog.pack();
         _dialog.setVisible(true);
     }
 
@@ -70,9 +80,13 @@ public class BarzahlungView
         _dialog.dispose();
     }
 
+    /**
+     * Erzeugt das Panel mit der Überschrift fuer das Programm.
+     */
     private JPanel erstelleUeberschriftPanel()
     {
         JPanel topPanel = new JPanel();
+        topPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 20, 0));
         JLabel label = new JLabel("Barzahlung", SwingConstants.CENTER);
 
         Font font = label.getFont()
@@ -87,64 +101,120 @@ public class BarzahlungView
     }
 
     /**
-     * Erzeugt das Panel mit der Überschrift fuer das Programm.
+     * Erzeugt das zentrale Panel mit den Bezahlungsfunktionen
+     * und einem Label für Fehlermeldungen.
      */
     private JPanel erstelleCenterPanel(Geldbetrag geldbetrag)
     {
         JPanel centerPanel = new JPanel();
+        centerPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
         centerPanel.setLayout(new BorderLayout());
-        JLabel label = new JLabel("Fehlermeldung", SwingConstants.CENTER);
 
-        JComponent barzahlungPanel = erstelleBarzahlungPanel(geldbetrag);
+        _fehlermeldungLabel = new JLabel("", SwingConstants.CENTER);
+        _fehlermeldungLabel
+            .setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        JComponent barzahlungPanel = erstelleBarzahlungPanel();
 
-        centerPanel.add(label, BorderLayout.SOUTH);
+        centerPanel.add(_fehlermeldungLabel, BorderLayout.SOUTH);
         centerPanel.add(barzahlungPanel, BorderLayout.CENTER);
 
         return centerPanel;
     }
 
     /**
-     * Erzeugt das Panel mit der Überschrift fuer das Programm.
+     * Erzeugt das Panel mit den Funktionen für die Barbezahlung.
      */
-    private JPanel erstelleBarzahlungPanel(Geldbetrag geldbetrag)
+    private JPanel erstelleBarzahlungPanel()
     {
-        JPanel barzahlungPanel = new JPanel();
-        barzahlungPanel.setLayout(new GridLayout(2, 3));
-        JLabel textPreisLabel = new JLabel("Preis", SwingConstants.CENTER);
-        JLabel preisLabel = new JLabel(geldbetrag.toString(),
-                SwingConstants.CENTER);
-        JLabel textEinzahlungLabel = new JLabel("Einzahlung",
-                SwingConstants.CENTER);
-        JTextField einzahlungTextField = new JTextField("Preis",
-                SwingConstants.CENTER);
-        JLabel textRestbetragLabel = new JLabel("Restbetrag",
-                SwingConstants.CENTER);
-        JLabel restbetragLabel = new JLabel("20€", SwingConstants.CENTER);
+        _barzahlungPanel = new JPanel();
+        _barzahlungPanel
+            .setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 10));
+        _barzahlungPanel.setLayout(new GridLayout(2, 3, 10, 10));
+        _textPreisLabel = new JLabel("Preis", SwingConstants.CENTER);
+        _preisWertLabel = new JLabel("", SwingConstants.CENTER);
+        _textEinzahlungLabel = new JLabel("Einzahlung", SwingConstants.CENTER);
+        _textEinzahlungLabel
+            .setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        _einzahlungTextField = new JTextField(SwingConstants.CENTER);
+        _textRestbetragLabel = new JLabel("Restbetrag", SwingConstants.CENTER);
+        _restbetragLabel = new JLabel("", SwingConstants.CENTER);
 
-        barzahlungPanel.add(textPreisLabel);
-        barzahlungPanel.add(textEinzahlungLabel);
-        barzahlungPanel.add(textRestbetragLabel);
-        barzahlungPanel.add(preisLabel);
-        barzahlungPanel.add(einzahlungTextField);
-        barzahlungPanel.add(restbetragLabel);
+        _barzahlungPanel.add(_textPreisLabel);
+        _barzahlungPanel.add(_textEinzahlungLabel);
+        _barzahlungPanel.add(_textRestbetragLabel);
+        _barzahlungPanel.add(_preisWertLabel);
+        _barzahlungPanel.add(_einzahlungTextField);
+        _barzahlungPanel.add(_restbetragLabel);
 
-        return barzahlungPanel;
+        return _barzahlungPanel;
     }
 
     /**
-     * Erzeugt das Panel mit dem Beenden-Button.
+     * Erzeugt das Panel mit dem Abbrechen- und Bezahlen-Button.
      */
     private JPanel erstelleBeendenPanel()
     {
         JPanel bottomPanel = new JPanel();
         bottomPanel.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
         bottomPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        JButton _abbrechenButton = new JButton("Abbrechen");
+        _abbrechenButton = new JButton("Abbrechen");
+        _bezahlenButton = new JButton("Bezahlen");
+        _bezahlenButton.setEnabled(false);
+
         bottomPanel.add(_abbrechenButton);
-        JButton _bezahlenButton = new JButton("Bezahlen");
         bottomPanel.add(_bezahlenButton);
 
         return bottomPanel;
+    }
+
+    /**
+     * Gibt das Textfeld für die Eingabe der Einzahlung zurück.
+     * 
+     * @return Das Textfeld für die Einzahlung.
+     */
+    public JTextField getEinzahlungTextField()
+    {
+        return _einzahlungTextField;
+    }
+
+    /**
+     * Gibt das Label für den Restbetrag zurück.
+     * 
+     * @return Das Label für den Restbetrag.
+     */
+    public JLabel getRestbetragLabel()
+    {
+        return _restbetragLabel;
+    }
+
+    /**
+     * Gibt das Label für den Preis zurück.
+     * 
+     * @return Das Label für den Preis.
+     */
+    public JLabel getPreisWertLabel()
+    {
+        return _preisWertLabel;
+    }
+
+    /**
+     * Gibt den "Bezahlen" Button zurück.
+     * 
+     * @return Der "Bezahlen" Button.
+     */
+    public JButton getBezahlenButton()
+    {
+        return _bezahlenButton;
+    }
+
+    /**
+     * Gibt den "Abbrechen" Button zurück.
+     * 
+     * @return Der "Abbrechen" Button.
+     */
+    public JButton getAbbrechenButton()
+    {
+        return _abbrechenButton;
     }
 
 }
